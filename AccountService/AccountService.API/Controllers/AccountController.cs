261e5f255aa80d7ea4223a35b34058f480b428b5
Nnkhanhh <nnkhanh2303@gmail.com>
2025-06-07 09:03:05 +0700

    Register

--- AccountService/AccountService.API/Controllers/AccountController.cs
+++ AccountService/AccountService.API/Controllers/AccountController.cs
@@ -1,11 +1,37 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AccountService.Application.Commands;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace AccountService.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AccountController : ControllerBase
+    public class AccountController(IMediator mediator) : ControllerBase
     {
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterAccountCommand command)
+        {
+            if (!ModelState.IsValid)
+            {
+                var firstError = ModelState.Values
+                .SelectMany(v => v.Errors)
+                .Select(e => e.ErrorMessage)
+                .FirstOrDefault();
+
+                return BadRequest(new { message = firstError });
+            }
+
+            try
+            {
+                var accountId = await mediator.Send(command);
+                return Ok(new { AccountId = accountId });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+
+        }
     }
 }
---
+++ AccountService/AccountService.Application/Handler/CommandHandler/RegisterAccountCommandHandler.cs
@@ -0,0 +1,55 @@
+﻿using MediatR;
+using AccountService.Application.Commands;
+using AccountService.Domain.Entity;
+using AccountService.Domain.IRepositories;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Org.BouncyCastle.Crypto.Generators;
+using DryIoc.ImTools;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+namespace AccountService.Application.Handler.CommandHandler
+{
+    public class RegisterAccountCommandHandler : IRequestHandler<RegisterAccountCommand, Guid>
+    {
+        private readonly IAccountRepositoryWrite _accountRepositoryWrite;
+
+        public RegisterAccountCommandHandler(IAccountRepositoryWrite accountRepositoryWrite)
+        {
+            _accountRepositoryWrite = accountRepositoryWrite;
+        }
+
+        public async Task<Guid> Handle(RegisterAccountCommand request, CancellationToken cancellationToken)
+        {
+            if (request.AccountPassword != request.ConfirmPassword)
+                throw new Exception("Password and Confirm Password do not match.");
+            if (await _accountRepositoryWrite.ExistsByUsernameAsync(request.AccountUsername))
+                throw new Exception("Username already exists.");
+            if (await _accountRepositoryWrite.ExistsByEmailAsync(request.AccountEmail))
+                throw new Exception("Email already exists.");
+
+            PasswordHasher<string> _passwordHasher = new PasswordHasher<string>();
+
+            var account = new Account
+            {
+                AccountId = Guid.NewGuid(),
+                AccountUsername = request.AccountUsername,
+                AccountPassword = _passwordHasher.HashPassword(null,request.AccountPassword),
+                AccountEmail = request.AccountEmail,
+                AccountFullName = request.AccountFullName,
+                AccountGender = request.AccountGender,
+                AccountRole = Role.USER.ToString(),
+                AccountStatus = Status.ACTIVE.ToString(),
+                AccountTicketRequest = 0
+            };
+
+            await _accountRepositoryWrite.AddAsync(account);
+
+            return account.AccountId;
+        }
+
+    }
+}
